caef6fb57775ca7ebe66d7fca3aacbf8ff09e183
Sandra Vilder <sandra.vilder@gmail.com>
2020-06-05 18:00:11 +0200

    Updates

---
+++ precache-manifest.f33cc6ed3531eec46fa14b73ad72cc50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6e9fe9cd1628f5aac7e8b9007ce715",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "be3acc499c4eb5b39c3c",
+    "url": "/tictactoe/static/css/main.885c1f76.chunk.css"
+  },
+  {
+    "revision": "dad74b667e464850e0ff",
+    "url": "/tictactoe/static/js/2.8dcbc98a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.8dcbc98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3acc499c4eb5b39c3c",
+    "url": "/tictactoe/static/js/main.4e589d97.chunk.js"
+  },
+  {
+    "revision": "1d8dada0581fd2a39a6b",
+    "url": "/tictactoe/static/js/runtime-main.d9939438.js"
+  }
+]);
