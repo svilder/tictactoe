79de02303af99d6af57c0485700f82cbdedbdbf9
Sandra Vilder <sandra.vilder@gmail.com>
2020-06-05 17:42:05 +0200

    Initial set up and styling

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Square = () => {
+  return (
+    <div className="">
+      
+      <img src="002-cancel.svg" alt="X" />
+    </div>
+  );
+}
+//        <img src="001-circle.svg" alt="0" />
+export default Square;
