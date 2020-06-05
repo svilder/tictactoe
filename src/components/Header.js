a5d8330c092e78dad050cb6151cac68f0fd4b9b5
Sandra Vilder <sandra.vilder@gmail.com>
2020-06-05 18:08:10 +0200

    gh-pages setup

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="d-flex justify-content-end w-100 mb-5">
+      <h1>Tic <em>tac</em> toe</h1>
+    </div>
+  );
+}
+
+export default Header;
